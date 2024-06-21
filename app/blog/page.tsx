5ffec29c6644b3ebe216a619ebcfccfe1842fb17
Edun Omatseye <edunomatseye@yahoo.com>
2024-06-21 14:59:12 +0100

    chore(avatar): added avatar component from nextui

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import { Autocomplete, AutocompleteItem, Avatar } from "@nextui-org/react";
 
 import { animals } from "./data";
 
@@ -33,6 +33,14 @@ export default function BlogPage() {
           )}
         </Autocomplete>
         <p className="text-default-500 text-small">Selected: {value}</p>
+      </div>
+      <div className="flex gap-3 items-center">
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+        <Avatar name="Junior" />
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+        <Avatar name="Jane" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+        <Avatar name="Joe" />
       </div>
     </div>
   );
