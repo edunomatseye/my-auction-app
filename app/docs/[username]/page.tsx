39343534602847bdf5f41121e521e81e7246ccdb
Edun Omatseye <edunomatseye@yahoo.com>
2024-07-04 12:15:44 +0100

    chore(route): added dynamic route to the project

---
+++ app/docs/[username]/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+//import getMovies from "./api/getMovies";
+import { Spinner } from "@nextui-org/spinner";
+
+import { getTodos } from "@/app/about/actions";
+import { title } from "@/components/primitives";
+
+export default function DocsPage({ params }: { params: { username: string } }) {
+  const { data, isLoading, isError } = useQuery({
+    queryFn: async () => await getTodos(),
+    queryKey: ["allTodos"], //Array according to Documentation
+  });
+
+  if (isLoading) return <Spinner />;
+  if (isError) return <div>Sorry There was an Error</div>;
+
+  return (
+    <div>
+      <h1 className={title()}>Docs</h1>
+      <h2>Using useQuery and populating the cache:- {params.username}</h2>
+      <span>{JSON.stringify(data, null, 4)}</span>
+    </div>
+  );
+}
