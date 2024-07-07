454a7682f65d34d07d77eabec34104e251dc3a40
Edun Omatseye <edunomatseye@yahoo.com>
2024-07-07 17:03:03 +0100

    chore(form): integrated React hook form and Next-UI Input and Button component to create loginForm

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ export default async function LoginPage() {
   return (
     <div>
       <h1 className={title()}>Login to Auction</h1>
-      <h2>Server componenet static rendered</h2>
+      <h2>Server component static rendered</h2>
 
       <LoginForm />
     </div>
--- components/loginForm.tsx
+++ components/loginForm.tsx
@@ -1,5 +1,62 @@
+"use client";
+
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Input, Button } from "@nextui-org/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import UserIcon from "./icon/UserIcon";
 
 export default function LoginForm() {
-  return <div>loginForm</div>;
+  const loginFormSchema = z.object({
+    email: z.string().email("Please provide a valid email address"),
+    password: z.string().min(8),
+  });
+
+  type LoginForm = z.infer<typeof loginFormSchema>;
+
+  const defaultValues = {
+    email: "olowo@bab.com",
+    password: "olowo@bab.com",
+  };
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues,
+  });
+
+  const onSubmit: SubmitHandler<LoginForm> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <div>loginForm</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          {...register("email", { required: true })}
+          className="max-w-xs"
+          placeholder="email"
+          type="email"
+        />
+        {errors.email && <span role="alert">{errors.email.message}</span>}
+        <Input
+          {...register("password", { required: true })}
+          className="max-w-xs"
+          placeholder="password"
+          type="password"
+        />
+        {errors.password && <span role="alert">{errors.password.message}</span>}
+        <Button color="success" startContent={<UserIcon />} type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
 }
