13c34c3ce75dd54c602ed426460c9eebd6f1b61d
Edun Omatseye <edunomatseye@yahoo.com>
2024-07-08 17:15:05 +0100

    chore(db): updated from db client to pool

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,16 +1,12 @@
 //import { drizzle } from "drizzle-orm/pg-proxy";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Client } from "pg";
+import { Pool } from "pg";
 
 import * as schema from "@/drizzle/schema/schema";
 import * as relations from "@/drizzle/schema/relations";
 
-export const client = new Client({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-(async () => {
-  // Your code with top-level await goes here
-  await client.connect();
-})();
-export const db = drizzle(client, { schema: { ...schema, ...relations } });
+export const db = drizzle(pool, { schema: { ...schema, ...relations } });
