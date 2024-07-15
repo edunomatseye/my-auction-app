59b4011235cdcecbcfb95182bd92530ce22289db
Edun Omatseye <edunomatseye@yahoo.com>
2024-07-15 21:38:09 +0100

    refactor(db): refactor db client from using pg to postgres and postgres-js

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,12 +1,21 @@
+import "dotenv/config";
 //import { drizzle } from "drizzle-orm/pg-proxy";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+// import { Pool } from "pg";
+
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import * as schema from "@/drizzle/schema/schema";
 import * as relations from "@/drizzle/schema/relations";
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+// export const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
 
-export const db = drizzle(pool, { schema: { ...schema, ...relations } });
+// export const db = drizzle(pool, { schema: { ...schema, ...relations } });
+
+const connectionString = process.env.DATABASE_URL!;
+
+// Disable prefetch as it is not supported for "Transaction" pool mode
+export const client = postgres(connectionString, { prepare: false });
+export const db = drizzle(client, { schema: { ...schema, ...relations } });
