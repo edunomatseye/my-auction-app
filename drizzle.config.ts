cef011007bc33eae2ed6aac8b6a788323f50aaea
Edun Omatseye <edunomatseye@yahoo.com>
2024-07-29 15:00:39 +0100

    chore(config): supabase, drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,15 @@
 import { loadEnvConfig } from "@next/env";
 import { defineConfig } from "drizzle-kit";
 
-const projectDir = process.cwd();
-
-loadEnvConfig(projectDir);
+loadEnvConfig(process.cwd());
 
 export default defineConfig({
-	schema: "./drizzle/schema/*",
-	out: "./drizzle/migrations",
-	dialect: "postgresql",
-	dbCredentials: {
-		url: process.env.SUPABASE_DIRECT_URL as string,
-	},
-	verbose: true,
-	strict: true,
+  schema: "./drizzle/schema/*",
+  out: "./drizzle/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.SUPABASE_DIRECT_URL as string,
+  },
+  verbose: true,
+  strict: true,
 });
